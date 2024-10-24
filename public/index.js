12baddb461d454d162379c3a73fd7d55ba6155fa
Kameron Livingston <waltercolorman@gmail.com>
2024-10-24 10:27:53 -0700

    js issues

--- public/index.js
+++ public/index.js
@@ -3,4 +3,7 @@ const now = new Date();
 const day = now.getDay(); // returns a number representing the day of the week, starting with 0 for Sunday
 const hours = now.getHours();
 const minutes = now.getMinutes();
-period.innerHTML= `Today is day ${day} and the time is ${hours}:${minutes}.`;
+
+while(true) {
+document.getElementById("period").innerHTML= `Today is day ${day} and the time is ${hours}:${minutes}.`;
+}
