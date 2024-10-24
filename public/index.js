1faa5b35bcb3bc63ab01a41a4578c62c6567ae98
Kameron Livingston <waltercolorman@gmail.com>
2024-10-24 13:49:05 -0700

    Finally founf the problem with the months and added comments

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
-const period = document.getElementById("period"); 
-const month = months[mont + 1]
+const period = document.getElementById("period");  //for later ;)
+
 var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December']
 
 setInterval(() => {
@@ -10,6 +10,7 @@ setInterval(() => {
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
-    document.getElementById("period").innerHTML = `Today is: ${month} ${day}, ${hours}:${minutes}:${seconds}`;
-    document.getElementById("title").innerHTML = `${month} ${day}`;
-}, 250);
+    const month = months[mont + 1] //selects the month from the array
+    document.getElementById("period").innerHTML = `Today is: ${month} ${day}, ${hours}:${minutes}:${seconds}`; //changes the H1
+    document.getElementById("title").innerHTML = `${month} ${day}`; //changes title
+}, 250); //updates display every 1/4 of a second
