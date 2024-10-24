beb26e0312c410c22362c6ecaeeb9a7d06c5c833
Kameron Livingston <waltercolorman@gmail.com>
2024-10-24 13:43:20 -0700

    Fixing the Live Update Month Names.

--- public/index.js
+++ public/index.js
@@ -1,8 +1,10 @@
 const period = document.getElementById("period"); 
+const month = months[mont + 1]
+var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December']
 
 setInterval(() => {
     const now = new Date();
-    const month = now.getMonth();
+    const mont = now.getMonth();
     const day = now.getDate();
     const hours = now.getHours();
     const minutes = now.getMinutes();
