7b31dea588fd3a0168b1b8c6e8134031b2119f68
Kameron Livingston <waltercolorman@gmail.com>
2024-10-24 10:32:18 -0700

    added seconds

--- public/index.js
+++ public/index.js
@@ -1,9 +1,10 @@
 const period = document.getElementById("period"); 
 const now = new Date();
-const day = now.getDay(); // returns a number representing the day of the week, starting with 0 for Sunday
+const day = now.getDate(); // returns a number representing the day of the week, starting with 0 for Sunday
 const hours = now.getHours();
 const minutes = now.getMinutes();
+const seconds = now.getSeconds();
 
-while(true == true) {
-document.getElementById("period").innerHTML= `Today is day ${day} and the time is ${hours}:${minutes}.`;
+while(true) {
+document.getElementById("period").innerHTML= `Today is day ${day} and the time is ${hours}:${minutes}:${seconds}.`;
 }
