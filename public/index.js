edb24621fa30a202c951a2908e4460ba6ed39a8c
Kameron Livingston <waltercolorman@gmail.com>
2024-10-24 12:05:14 -0700

    i just wanna fix this already

--- public/index.js
+++ public/index.js
@@ -3,14 +3,11 @@ const period = document.getElementById("period");
 setInterval(() => {
     const now = new Date();
     const month = now.getMonth();
-    const monthname = months[month];
     const day = now.getDate();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
-    const months = [ "January", "February", "March", "April", "May", "June", 
-        "July", "August", "September", "October", "November", "December" ];
 
-    document.getElementById("period").innerHTML = `Today is: ${monthname} ${day}, ${hours}:${minutes}:${seconds}`;
-    document.getElementById("title").innerHTML = `${monthname} ${day}`;
+    document.getElementById("period").innerHTML = `Today is: ${month} ${day}, ${hours}:${minutes}:${seconds}`;
+    document.getElementById("title").innerHTML = `${month} ${day}`;
 }, 250);
