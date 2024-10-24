353ccc9d66fa180876e92c36a957d7e9c75ef978
Kameron Livingston <waltercolorman@gmail.com>
2024-10-24 10:36:05 -0700

    added setInterval

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
 const period = document.getElementById("period"); 
+
+setInterval(() => {
 const now = new Date();
 const day = now.getDate(); // returns a number representing the day of the week, starting with 0 for Sunday
 const hours = now.getHours();
 const minutes = now.getMinutes();
 const seconds = now.getSeconds();
-
-while(true) {
 document.getElementById("period").innerHTML= `Today is day ${day} and the time is ${hours}:${minutes}:${seconds}.`;
-}
+}, 500);
