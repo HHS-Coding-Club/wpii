0d4ddc0beb3bb2d184284825daa9f129a77d27af
Kameron Livingston <waltercolorman@gmail.com>
2024-10-24 10:39:57 -0700

    Fixed date & time

--- public/index.js
+++ public/index.js
@@ -2,9 +2,10 @@ const period = document.getElementById("period");
 
 setInterval(() => {
 const now = new Date();
-const day = now.getDate(); // returns a number representing the day of the week, starting with 0 for Sunday
+const month = now.getMonth();
+const day = now.getDate();
 const hours = now.getHours();
 const minutes = now.getMinutes();
 const seconds = now.getSeconds();
-document.getElementById("period").innerHTML= `Today is day ${day} and the time is ${hours}:${minutes}:${seconds}.`;
+document.getElementById("period").innerHTML= `Today is: ${month} ${day}, ${hours}:${minutes}:${seconds}`;
 }, 500);
