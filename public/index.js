91d8d7b5db837014ffe747c94d310d233c71b90b
Kameron Livingston <waltercolorman@gmail.com>
2024-10-24 11:46:08 -0700

    Added month names

--- public/index.js
+++ public/index.js
@@ -1,12 +1,16 @@
 const period = document.getElementById("period"); 
 
 setInterval(() => {
-const now = new Date();
-const month = now.getMonth();
-const day = now.getDate();
-const hours = now.getHours();
-const minutes = now.getMinutes();
-const seconds = now.getSeconds();
-document.getElementById("period").innerHTML= `Today is: ${month} ${day}, ${hours}:${minutes}:${seconds}`;
-document.getElementById("title").innerHTML= `${month} ${day}`;
+    const now = new Date();
+    const month = now.getMonth();
+    const monthname = months[month];
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    var months = [ "January", "February", "March", "April", "May", "June", 
+        "July", "August", "September", "October", "November", "December" ];
+
+    document.getElementById("period").innerHTML = `Today is: ${monthname} ${day}, ${hours}:${minutes}:${seconds}`;
+    document.getElementById("title").innerHTML = `${monthname} ${day}`;
 }, 250);
