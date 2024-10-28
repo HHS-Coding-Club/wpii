4d98ec4182d7bb2b7d3c87042e9b03d7eaca65f5
Kameron Livingston <waltercolorman@gmail.com>
2024-10-28 08:59:13 -0700

    Fixing Time

--- public/index.js
+++ public/index.js
@@ -37,55 +37,55 @@ setInterval(() => {
     }
 
     if(schedule1.includes(dow)) {
-        if(hours < 6 && minutes < 55) {
+        if(hours < 5 && minutes < 55) {
             var selector = "Before School"
         };
-        if(hours > 6 && minutes > 55) {
+        if(hours > 5 && minutes > 55) {
             var selector = "0 Hour"
         };
-        if(hours > 7 && minutes > 50) {
+        if(hours > 6 && minutes > 50) {
             var selector = "Before 1st"
         };
-        if(hours > 7 && minutes > 55) {
+        if(hours > 6 && minutes > 55) {
             var selector = "1st Hour"
         };
-        if(hours > 8 && minutes > 50) {
+        if(hours > 7 && minutes > 50) {
             var selector = "Before 2nd"
         };
-        if(hours > 8 && minutes > 55) {
+        if(hours > 7 && minutes > 55) {
             var selector = "2nd Hour"
         };
-        if(hours > 9 && minutes > 50) {
+        if(hours > 8 && minutes > 50) {
             var selector = "Before 3rd"
         };
-        if(hours > 9 && minutes > 55) {
+        if(hours > 8 && minutes > 55) {
             var selector = "3rd Hour"
         };
-        if(hours > 10 && minutes > 55) {
+        if(hours > 9 && minutes > 55) {
             var selector = "Before 4th / 1st Lunch"
         };
-        if(hours > 11 && minutes > 25) {
+        if(hours > 10 && minutes > 25) {
             var selector = "4th Hour"
         };
-        if(hours > 11 && minutes > 55) {
+        if(hours > 10 && minutes > 55) {
             var selector = "4th Hour / 2nd Lunch"
         };
-        if(hours > 12 && minutes > 25) {
+        if(hours > 11 && minutes > 25) {
             var selector = "Before 5th"
         };
-        if(hours > 12 && minutes > 30) {
+        if(hours > 11 && minutes > 30) {
             var selector = "5th Hour"
         };
-        if(hours > 13 && minutes > 25) {
+        if(hours > 12 && minutes > 25) {
             var selector = "Before 6th"
         };
-        if(hours > 13 && minutes > 30) {
+        if(hours > 12 && minutes > 30) {
             var selector = "6th Hour"
         };
-        if(hours > 14 && minutes > 25) {
+        if(hours > 13 && minutes > 25) {
             var selector = "End of school / Clubs"
         };
-        if(hours > 15 && minutes > 25) {
+        if(hours > 14 && minutes > 25) {
             var selector = "End of school"
         };
 
